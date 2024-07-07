06970b8d2a28f67fd44681f53eb9bf85f7107fa9
takamitsu-iida <takamitsu.iida@gmail.com>
2024-07-07 16:01:43 +0900

    minor fix

--- bin/binary_search_tree.py
+++ bin/binary_search_tree.py
@@ -18,7 +18,8 @@
 
 
 参考文献
-アルゴリズム図鑑 増補改訂版 石田保輝 宮崎修一著 翔泳社
+  アルゴリズム図鑑 増補改訂版 石田保輝 宮崎修一著 翔泳社
+  https://www.shoeisha.co.jp/book/detail/9784798172439
 
 """
 
@@ -220,7 +221,7 @@ def tree_height(root: BinaryTreeNode) -> int:
     return (max(tree_height(root.left), tree_height(root.right)) + 1) if root else 0
 
 
-def print_binary_tree(root: BinaryTreeNode, level=0):
+def print_binary_tree_h(root: BinaryTreeNode, level=0):
     """preorder探索でノードを表示する
 
     Args:
@@ -229,14 +230,52 @@ def print_binary_tree(root: BinaryTreeNode, level=0):
     if root == None:
         return
 
-    print_binary_tree(root.left, level + 1)
+    print_binary_tree_h(root.left, level + 1)
     print(' ' * 4 * level + '-> ' + str(root.data))
-    print_binary_tree(root.right, level + 1)
+    print_binary_tree_h(root.right, level + 1)
+
+
+def print_binary_tree_v(root: BinaryTreeNode):
+
+    def _get_coloum(height):
+        if height == 1:
+            return 1
+        # heightを一つ減らすたびに2倍+1にしていく
+        return _get_coloum(height-1) *2 + 1
+
+
+    def _create_print_matrix_preorder(matrix, root, col, row, height):
+        if root is None:
+            return
+        #
+        # preorder探索での処理
+        #
+
+        # 自分の位置に値を入れる
+        matrix[row][col] = root.data
+
+        _create_print_matrix_preorder(matrix, root.left, col - pow(2, height-2), row +1, height -1)
+        _create_print_matrix_preorder(matrix, root.right, col + pow(2, height-2), row +1, height -1)
+
+
+    height = tree_height(root)
+    column = _get_coloum(height)
+
+    # ' 'で初期化した行列を作成
+    matrix = [[' ']*column for _ in range(height)]
+
+    # rootの位置は、x=行列の中央、y=0として、preorder探索で値を入れていく
+    _create_print_matrix_preorder(matrix, root, column//2, 0, height)
+
+    for row in matrix:
+        for col in row:
+            print(col, end=' ')
+        print('')
 
 
 def test_binary_tree():
 
-    # アルゴリズム図鑑にかかれている例を使ってみる
+    # 参考文献のアルゴリズム図鑑にかかれている例を使ってみる
     data_list = [15, 9, 23, 3, 12, 17, 28, 8]
 
     # 15を頂点にして、残りは上記のリストを使って二分探索木を作成する
@@ -244,66 +283,73 @@ def test_binary_tree():
     for data in data_list:
         insert_binary_tree(root, data)
 
-    print("--- Binary Tree ---")
-    print_binary_tree(root)
+    # 以下のような二分探索木ができる
+    #       15
+    #     /    \
+    #   9       23
+    #  / \     /  \
+    # 3   12  17   28
+    #  \
+    #   8
+
+    print("--- Binary Tree From Left to Right---")
+    print_binary_tree_h(root)
     print('')
 
+    # ツリーの深さを表示
     print("--- Tree Hight---")
     print(tree_height(root))
     print('')
 
+    # preorder探索でノードを表示
+    # 15, 9, 3, 8, 12, 23, 17, 28 の順に表示される
     print("--- Preorder Traversal ---")
-    traverse_preorder(root, lambda node: print(node.data))
-    print('')
+    traverse_preorder(root, lambda node: print(node.data, end=' '))
+    print('\n'*2)
 
+    # postorder探索でノードを表示
+    # 8, 3, 12, 9, 17, 28, 23, 15 の順に表示される
     print("--- Postorder Traversal ---")
-    traverse_postorder(root, lambda node: print(node.data))
+    traverse_postorder(root, lambda node: print(node.data, end=' '))
+    print('\n'*2)
+
+    # 操作前
+    print("--- Before Operation ---")
+    print_binary_tree_h(root)
     print('')
 
     # 1を追加する
     print("--- insert 1 ---")
     insert_binary_tree(root, 1)
-    print_binary_tree(root)
+    print_binary_tree_h(root)
     print('')
 
     # 4を追加する
     print("--- insert 4 ---")
     insert_binary_tree(root, 4)
-    print_binary_tree(root)
+    print_binary_tree_h(root)
     print('')
 
     # 28を削除する
     # 28は末端に位置している
     print("--- delete 28 ---")
     delete_binary_tree(root, 28)
-    print_binary_tree(root)
+    print_binary_tree_h(root)
     print('')
 
     # 8を削除する
     # 8は中間で子を一つだけ持つ
     print("--- delete 8 ---")
     delete_binary_tree(root, 8)
-    print_binary_tree(root)
+    print_binary_tree_h(root)
     print('')
 
     # 9を削除する
     # 9は中間で子を二つ持つ
     print("--- delete 9 ---")
     delete_binary_tree(root, 9)
-    print_binary_tree(root)
+    print_binary_tree_h(root)
     print('')
-
-    print("--- Preorder Traversal ---")
-    print([node.data for node in preorder(root)])
-
-    print("--- Inorder Traversal ---")
-    print([node.data for node in inorder(root)])
-
-    print("--- Postorder Traversal ---")
-    print([node.data for node in postorder(root)])
-
-    print("--- Level Order Traversal ---")
-    print([node.data for node in level_order(root)])
 
 
 if __name__ == '__main__':
